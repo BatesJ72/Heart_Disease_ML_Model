9b1d62bbbc62fc1dafa15b7b3ea7d7339db865ce
Jessica Bates <bates.jessica72@gmail.com>
2021-01-11 20:55:14 -0600

    data exploration

---
+++ data.py
@@ -0,0 +1,15 @@
+# import csv
+import pandas as pd
+
+# Read in the data
+with open('heart_disease_data.csv', 'r') as file:
+    # data = file.read()
+    # data = file.readlines()
+    # data = file.readline()
+    data = pd.read_csv('heart_disease_data.csv')
+
+# Check Data
+# print(data)
+# print(data.dtypes)
+# print(data.describe())
+
