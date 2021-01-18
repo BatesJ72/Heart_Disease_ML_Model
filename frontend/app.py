abc6a46616130887b9db96f4fa9289a9077cc158
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2021-01-18 13:28:52 -0600

    committing from saturday

--- frontend/app.py
+++ frontend/app.py
@@ -4,21 +4,32 @@ from flask import Flask, jsonify, render_template, request, make_response, url_f
 # import json
 import pandas as pd
 import numpy as np
-# import scaler
+import joblib
 
 app = Flask(__name__)
 
 # model = 
 
-def predict_disease(input_data, model):
-    x = input_data
-#  pass x to the model
+clf = joblib.load("../backend/clf.joblib")
+
+model_input =[]
+
+def predict_heart_disease(input_data):
+    for i in input_data:
+        model_input.append(i)
+    return clf.predict([model_input])[0]
+
+input_data = []
+
+
+# def predict_disease(input_data, model):
+#     x = input_data
+# #  pass x to the model
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     if request.method == "POST": 
-        
 
         age = request.form["age"]
         sex = request.form["sex"]
@@ -29,9 +40,25 @@ def index():
         thalach = request.form["thalach"]
         exang = request.form["exang"]
 
-        input_data = [age, sex, cp, fbs, trestbps, chol, thalach, exang]
+        input_data.append([age, sex, cp, fbs, trestbps, chol, thalach, exang])
         
-        print(input_data)
+        # for i in input_data:
+        #     for d in i:
+        #         print(type(d))
+    
+        prediction = predict_heart_disease(input_data)
+
+        print({
+            "age": int(age), 
+            "sex": bool(sex), 
+            "cp": int(cp), 
+            # "trestbps": int(trestbps), 
+            # "chol": int(chol), 
+            "fbs": int(fbs), 
+            # "thalach": int(thalach), 
+            "exang": int(exang),
+            "prediction": bool(prediction)
+    })
 
 #   ml function
 
@@ -43,6 +70,15 @@ def index():
 def visual():
     return render_template("visual.html")
 
+
+@app.route("/predict")
+def new_route():
+
+    return predict_heart_disease(input_data)
+
+    
+
+     
 
 if __name__ == "__main__":
     app.run(debug=True)
