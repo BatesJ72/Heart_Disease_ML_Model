bf8ca29378e9076c33ba15421dd96d2ed1c3fb36
Jessica Bates <bates.jessica72@gmail.com>
2021-01-25 19:48:54 -0600

    reorganized files

---
+++ backend/model_rf.py
@@ -0,0 +1,66 @@
+# import packages
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+
+import joblib
+
+# read in data
+df = pd.read_csv("heart_disease_data.csv")
+
+# drop unused columns
+df = (
+    df.drop("restecg", axis=1)
+    .drop("oldpeak", axis=1)
+    .drop("slope", axis=1)
+    .drop("ca", axis=1)
+    .drop("thal", axis=1)
+)
+
+# rename exang: exercise induced angina (1 = yes; 0 = no)
+df = df.assign(exang=lambda df: df["exang"].replace({0: "no", 1: "yes"}))
+
+# rename sex
+df = df.assign(sex=lambda df: df["sex"].replace({0: "female", 1: "male"}))
+
+# rename fbs: (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+df = df.assign(fbs=lambda df: df["fbs"].replace({0: "no", 1: "yes"}))
+
+# rename cp: chest pain type
+df = df.assign(
+    cp=lambda df: df["cp"].replace(
+        {
+            0: "typical angina",
+            1: "atypical angina",
+            2: "non-anginal pain",
+            3: "asymptomatic",
+        }
+    )
+)
+
+# Set x and y
+target = "condition"
+
+y = df["condition"]
+X = df.drop("condition", axis=1)
+
+cf = ColumnTransformer(
+    [
+        ("numerical", "passthrough", ["age", "trestbps", "chol", "thalach"],),
+        ('"categorical"', OneHotEncoder(drop="first"), ["sex", "cp", "fbs", "exang"]),
+    ]
+)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
+
+# train model 
+rf_pipeline = make_pipeline(cf, RandomForestClassifier(n_estimators=10000, criterion="gini"))
+
+rf_pipeline.fit(X, y)
+
+# create joblib file
+joblib.dump(rf_pipeline, "clf.joblib")
--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,5 @@
 import os
 from flask import Flask, jsonify, render_template, request, make_response, url_for, redirect
-# import requests
-# import json
 import pandas as pd
 import numpy as np
 import joblib
@@ -9,22 +7,14 @@ import plotly
 
 app = Flask(__name__)
 
-
+# ML Model
 clf = joblib.load(os.path.join("..","backend", "clf.joblib"))
-
 def predict_heart_disease(data_input):
     return clf.predict(data_input)
 
 
-# def predict_heart_disease(age, sex, cp, trestbps, chol, fbs, thalach, exang):
-#     model_input = [age, sex, cp, trestbps, chol, fbs, thalach, exang]
-#     return clf.predict([model_input])
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # input_data = []
-    # data_input = []
-
     if request.method == "POST": 
 
         age = request.form["age"]
@@ -36,117 +26,6 @@ def index():
         thalach = request.form["thalach"]
         exang = request.form["exang"]
 
-        print(age)
-        print(sex)
-        print(cp)
-        print(fbs)
-        print(trestbps)
-        print(chol)
-        print(thalach)
-        print(exang)
-
-
-        # input_data.append([age, sex, cp, fbs, trestbps, chol, thalach, exang])
-        
-        # data_input = pd.DataFrame(np.array(input_data).reshape(8,1), columns = list("age", "sex", "cp", "fbs", "trestbps", "chol", "thalach", "exang"))
-
-        # input_data = pd.DataFrame(
-        #     {
-        #         "age": 65,
-        #         "sex": "male",
-        #         "cp": "asymptomatic",
-        #         "trestbps": 110,
-        #         "chol": 264,
-        #         "fbs": "yes",
-        #         "thalach": 131,
-        #         "exang": "yes",
-        #     },
-        #     index=[0],
-        # )
-        
-        # input_data.update(
-        #     {
-        #         "age": age,
-        #         "sex": sex,
-        #         "cp": cp,
-        #         "trestbps": trestbps,
-        #         "chol": chol,
-        #         "fbs": fbs,
-        #         "thalach": thalach,
-        #         "exang": exang,
-        #     })
-        
-        
-        # for i in input_data:
-        #     for d in i:
-        #         print(type(i))
-        #         # e = int(d)
-        #         print(type(d))
-        #         # data_input.append(e)
-        #         print(i)
-        #         print(d)
-        
-        
-        # for i in input_data:
-        #     print(type(i))
-        #     print(i)
-            
-    
-        
-        # for i in input_data:
-        #     for key, value in i.items():
-        #         if key == 'age':
-        #             value = int(value)
-        #             data_input.update(key, value)
-        #         if key == "sex":
-        #             data_input.update(key, value)
-        #         if key == "cp": 
-        #             data_input.update(key, value)
-        #         if key == "trestbps":
-        #             value = int(value)
-        #             data_input.update(key, value)
-        #         if key == "chol":
-        #             value = int(value)
-        #             data_input.update(key, value)
-        #         if key == "fbs": 
-        #             data_input.update(key, value)
-        #         if key == "thalach":
-        #             value = int(value)
-        #             data_input.update(key, value)
-        #         if key == "exang": 
-        #             data_input.update(key, value)
-    
-        # data_input = pd.DataFrame(input_data)
-
-        # print(f"print 1 {input_data}")
-        # print(([input_data])[0])
-        # for i in input_data:
-        #     for d in i:
-        #         # print(type(d))
-        #         e = d
-        #         # print(type(d))
-        #         data_input.append(e)
-
-        # print(f"print 2 {data_input}")
-
-        # print(input_data)
-        # print(data_input)
-
-        data_input = (pd.DataFrame(
-        {
-            "age": age, 
-            "sex": sex, 
-            "cp": cp, 
-            "fbs": fbs, 
-            "trestbps": trestbps, 
-            "chol": chol, 
-            "thalach": thalach, 
-            "exang": exang,
-        },
-        index=[0],
-        ))
-        print(data_input)
-        # data = predict_heart_disease(age, sex, cp, trestbps, chol, fbs, thalach, exang)
         data = predict_heart_disease(pd.DataFrame(
         {
             "age": age, 
@@ -160,9 +39,7 @@ def index():
         },
         index=[0],
     ))
-        # data = predict_heart_disease(data_input)
 
-        print(data)
 
         if data == 1:
             data = "You are at risk! Take heed!"
@@ -178,15 +55,6 @@ def index():
 def visual():
     return render_template("visual.html")
 
-
-@app.route("/predict")
-def new_route():
-
-    return predict_heart_disease(input_data)
-
-    
-
-     
 
 if __name__ == "__main__":
     app.run(debug=True)
